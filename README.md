0d9b40f7a1ddc84fa25c36eeaa1943892d392e53
Rahima-Akter <rahimaakter1527@gmail.com>
2024-10-19 08:58:18 -0700

    project complete

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # phone-hunt-js
+
+#about this project
+This is a smart gadgets website, where people can search anything they like. And they can also see the full details about that specific product.
+
+#Main features of this project
+1.can search products Dynamically or on button click
+2.fetched all the data from an API.
+3.with click on the Details button a modal will apear with the product details
+4.with show all button click all the available products will appear.
+5.ES6
+6.fully responsive
+7.a spinner will appear on every loading and disappear after 2s.
--- index.js
+++ index.js
@@ -33,23 +33,26 @@ const showPhones = (phones, searchtext) => {
     };
 
     currenPhones = phones;
-    const phonesToShow = showAll ? phones : phones.slice(0,8);
+    const phonesToShow = showAll ? phones : phones.slice(0, 8);
     phonesToShow.forEach(phone => {
         const div = document.createElement('div');
+        div.classList.add('shadow-md', 'rounded-lg', 'p-6', 'bg-white')
         div.innerHTML = `
-            <div class=" shadow-md rounded-lg p-6">
-                <img src="${phone.image}" alt="Product 1" class="w-[80%] mx-auto h-40 object-fill rounded-md mb-4">
+                <img src="${phone.image}" alt="Product 1" class="w-[50%] md:w-[70%] mx-auto h-40 md:h-44  rounded-md mb-4">
                 <h3 class="text-lg mb-2 font-bold">${phone.phone_name}</h3>
-                <p class="text-gray-600 mb-4">Compact size with the same performance.</p>
-                <p class="text-black font-bold mb-4">$99</p>
-                <button class="bg-blue-500 text-white py-2 px-4 rounded-lg w-full hover:bg-blue-600">Buy
+                <p class="text-gray-600 mb-2">Compact size with the same performance.</p>
+                <p class="text-black font-bold ">$99</p>
+                <div class="divider my-2"></div>
+                <div class="flex justify-evenly">
+                    <button class="bg-blue-500 text-white rounded-lg hover:bg-blue-600 btn md:btn-sm">Buy
                     Now</button>
-            </div>
+                    <button onclick="modalApiFetch('${phone.slug}')" class="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 btn md:btn-sm">Details</button>
+                </div>
         `;
         phoneContainer.append(div);
     });
 
-    if(phones.length > 8){
+    if (phones.length > 8) {
         setTimeout(function () {
             document.getElementById('showAll').classList.remove('hidden')
         }, 2000);
@@ -90,12 +93,51 @@ const loader = () => {
 
     loader.classList.remove('hidden');
     productContainer.classList.add('hidden');
-    
+
     setTimeout(function () {
         loader.classList.add('hidden');
         productContainer.classList.remove('hidden');
     }, 2000);
     // document.getElementById('searchBtn').addEventListener('click', loader)
 };
+
+//modal api fetch
+const modalApiFetch = async (slug) => {
+    try {
+        const res = await fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
+        const data = await res.json()
+        modal(data.data)
+    }
+    catch (err) {
+        console.log('error:', err)
+    }
+};
+
+// show modal
+const modal = (id) => {
+    document.getElementById('detailsModal').showModal();
+    const ModalContainer = document.getElementById('modal-content');
+    ModalContainer.innerHTML = '';
+    const div = document.createElement('div');
+    div.innerHTML =` 
+        <div class="p-2">
+            <img src="${id.image}" alt="${id.name}" class="md:w-[40%] w-[60%] mx-auto">
+            <p class="font-bold text-2xl my-4 text-center">${id.name}</p>
+            <p class="text-sm mt-1"><span class="font-semibold text-[16px]">Storage:</span> ${id.mainFeatures.storage}</p>
+            <p class="text-sm mt-1"><span class="font-semibold text-[16px]">display Size:</span> ${id.mainFeatures.displaySize}</p>
+            <p class="text-sm mt-1"><span class="font-semibold text-[16px]">Chipset:</span> ${id.mainFeatures.chipSet}</p>
+            <p class="text-sm mt-1"><span class="font-semibold text-[16px]">Memory:</span> ${id.mainFeatures.memory}</p>
+            <p class="text-sm mt-1"><span class="font-semibold text-[16px]">Release Date:</span> ${id.releaseDate}</p>
+            <p class="text-sm mt-1"><span class="font-semibold text-[16px]">Brand:</span> ${id.brand}</p>
+            <p class="text-sm mt-1"><span class="font-semibold text-[16px]">GPS:</span> ${id.others.GPS}</p>
+        </div>
+        <div class="modal-action">
+            <form method="dialog" class="relative w-full">
+                <button class="btn bg-red-500  w-full text-white font-bold">Close</button>
+            </form>
+        </div>
+    `;
+    ModalContainer.append(div)
+}
 
 showPhones([], '');
